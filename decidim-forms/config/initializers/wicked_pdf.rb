081adb38420c2b184dbebb5807d5d4409db3085a
Fran Bolívar <francisco.bolivar.lupianez@gmail.com>
2025-04-23 11:51:01 +0200

    Downgrade wicked_pdf to 2.7.0 (#47)

--- decidim-forms/config/initializers/wicked_pdf.rb
+++ decidim-forms/config/initializers/wicked_pdf.rb
@@ -14,13 +14,14 @@ require "wicked_pdf"
 #
 # https://github.com/mileszs/wicked_pdf/blob/master/README.md
 
-WickedPdf.configure do |config|
+WickedPdf.config = {
   # Path to the wkhtmltopdf executable: This usually is not needed if using
   # one of the wkhtmltopdf-binary family of gems.
+  # exe_path: '/usr/local/bin/wkhtmltopdf',
   #   or
-  # config.exe_path = '/usr/local/bin/wkhtmltopdf',
+  # exe_path: Gem.bin_path("wkhtmltopdf-binary", "wkhtmltopdf")
 
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
-  # config.layout = 'pdf.html'
-end
+  # layout: 'pdf.html',
+}
