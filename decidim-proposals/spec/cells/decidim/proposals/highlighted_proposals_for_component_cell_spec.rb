b6b3829d13c105d5da42b9db02ed9b269a4ee351
Fran Bolívar <francisco.bolivar@nazaries.com>
2026-01-21 16:45:04 +0100

    Remove repeated specs

--- decidim-proposals/spec/cells/decidim/proposals/highlighted_proposals_for_component_cell_spec.rb
+++ decidim-proposals/spec/cells/decidim/proposals/highlighted_proposals_for_component_cell_spec.rb
@@ -46,26 +46,6 @@ describe Decidim::Proposals::HighlightedProposalsForComponentCell, type: :cell d
     end
   end
 
-  context "when no published proposals" do
-    let!(:proposal) { create(:proposal, :unpublished, title: { en: "A nice title" }, component: model) }
-
-    it "renders the proposals" do
-      expect(subject).to have_no_content("A nice title")
-      expect(subject).to have_no_content("Last proposals")
-      expect(subject).to have_no_css(".card__list-title", count: 1)
-    end
-  end
-
-  context "when proposals are hidden" do
-    let!(:proposal) { create(:proposal, :hidden, title: { en: "A nice title" }, component: model) }
-
-    it "renders the proposals" do
-      expect(subject).to have_no_content("A nice title")
-      expect(subject).to have_no_content("Last proposals")
-      expect(subject).to have_no_css(".card__list-title", count: 1)
-    end
-  end
-
   context "with proposals" do
     let!(:proposal2) { create(:proposal, title: { en: "Another nice title" }, component: model) }
 
