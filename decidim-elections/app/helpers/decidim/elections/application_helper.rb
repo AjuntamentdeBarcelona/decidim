f4cc25da57c0373fd47b817f7a0b1a4585084a37
Andrés Pereira de Lucena <andreslucena@users.noreply.github.com>
2026-04-29 16:57:28 +0200

    Escape elections questions titles (#16659)

--- decidim-elections/app/helpers/decidim/elections/application_helper.rb
+++ decidim-elections/app/helpers/decidim/elections/application_helper.rb
@@ -43,7 +43,7 @@ module Decidim
           if question.max_choices.present? && question.question_type == "multiple_option"
             title += " (#{t("decidim.elections.votes.question.max_choices", count: question.max_choices)})"
           end
-          title.html_safe
+          title
         end
       end
 
